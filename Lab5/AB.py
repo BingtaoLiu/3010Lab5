e4df1e20fbb792ab14433b41264baf2ab058984a
Aaditya <aaditya.sharma@carleton.ca>
2020-10-20 15:33:14 -0400

    Initial commit A.py to branch ideaA and modified AB.py

--- Lab5/AB.py
+++ Lab5/AB.py
@@ -1,6 +1,8 @@
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
+from A import * # Line is added by Team Member A
 
 def function_AB():
+    function_A()
     print ("AB")
  
