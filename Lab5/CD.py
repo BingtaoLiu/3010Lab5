74d1f622b03b55c1f387aeea1f69d5046584b640
BingtaoLiu <71525740+BingtaoLiu@users.noreply.github.com>
2020-10-20 18:23:09 +0800

    sysc 3010 lab 5

--- Lab5/CD.py
+++ Lab5/CD.py
@@ -1,8 +1,11 @@
 # SYSC3010 Lab 5 - CD.py
-#     The file to be jointly edited by Team Members C and D
+# The file to be jointly edited by Team Members C and D
+
+
+from D import * # Line is added by Team Member D
+from C import *
 
 def function_CD():
-    print ("CD")
-    
+	function_D() # Line is added by Team Memeber D
+	print ("CD")
 
-    
