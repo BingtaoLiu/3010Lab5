79a25a0ad1671f188341d1f1a99958ccf265b0cb
BingtaoLiu <71525740+BingtaoLiu@users.noreply.github.com>
2020-11-13 00:16:04 -0500

    Add files via upload

---
+++ Project/sensehat.py
@@ -0,0 +1,72 @@
+import random
+import time
+import httplib
+import urllib
+import requests
+import json
+
+
+
+class SenseHat:
+    def __init__(self,readAPI,writeAPI):
+        self.temp = 0
+        self.humidity = 0
+        self.readAPI = readAPI
+        self.writeAPI = writeAPI
+        self.userID = '3'
+        self.sensorID = '4'
+    
+    def establishConnection(self):
+        self.writeTS(self.userID+','+self.sensorID+','+'TRUE')
+        connectionEstablished = False
+        while connectionEstablished == False:
+            time.sleep(10)
+            result = self.readTS()
+            if result == self.userID+','+self.sensorID+','+'FALSE':
+                print("Connection Established")
+                connectionEstablished = True
+            else:
+                print("Connection Failed")
+                connectionEstablished = False
+    
+    def readingString(self):
+        self.temp = self.readtemp()
+        self.humidity = self.readhumidity()
+        return self.userID+","+self.sensorID+","+str(self.temp)+","+str(self.humidity)
+    
+    def writeTS(self,fieldString):
+        params = urllib.urlencode({'field1': fieldString, 'key':self.writeAPI }) 
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print(fieldString)
+            print(response.status, response.reason)
+            data = response.read()
+            conn.close()
+        except:
+            print("connection failed")
+        
+    def readTS(self):
+        data = requests.get(self.readAPI).json()
+        #feeds->last entry ->field1
+        result = data['feeds'][len(data['feeds'])-1]['field1']
+        return result
+    
+    def readtemp(self):
+        return random.random()*100
+    
+    def readhumidity(self):
+        return random.random()*100
+    
+if __name__ == '__main__':
+    readAPI ='https://api.thingspeak.com/channels/1161317/fields/1.json?api_key=' + 'IFI077MYY53P4HR4' + '&results='
+    writeAPI = "P74NDBTW9HDK0JDU"
+    SenseHatPi = SenseHat(readAPI,writeAPI)
+    SenseHatPi.establishConnection()
+    while True:
+        time.sleep(10)
+        SenseHatPi.writeTS(SenseHatPi.readingString())
+        print(SenseHatPi.readTS())
+        
