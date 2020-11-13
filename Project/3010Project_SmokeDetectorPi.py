d9b9cfdfe4092d44a5e8fd523cbc07795fdb48a3
Andrew Cowan <andycowan@cmail.carleton.ca>
2020-11-12 20:54:27 -0500

    feat: adding smoke detector code

---
+++ Project/3010Project_SmokeDetectorPi.py
@@ -0,0 +1,66 @@
+import random
+import time
+import httplib
+import urllib
+#import urllib.request
+import requests
+import json
+
+class MQ2:
+    def __init__(self,readAPI,writeAPI):
+        self.methane = 0
+        self.CO2 = 0
+        self.readAPI = readAPI
+        self.writeAPI = writeAPI
+        self.userID = '1'
+        self.sensorID = '2'
+    
+    def establishConnection(self):
+        self.writeTS(self.userID+','+self.sensorID+','+'TRUE')
+        time.sleep(5)
+        result = self.readTS()
+        if result == self.userID+','+self.sensorID+','+'TRUE':
+            print("Connection Established")
+        else:
+            print("Connection Failed")
+    
+    def readingString(self):
+        self.CO2 = self.readCO2()
+        self.methane = self.readMethane()
+        return self.userID+","+self.sensorID+","+str(self.CO2)+","+str(self.methane)
+    
+    def writeTS(self,fieldString):
+        params = urllib.urlencode({'field1': fieldString, 'key':self.writeAPI }) 
+        #print(params)
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+            response = conn.getresponse()
+            print(fieldString)
+            print(response.status, response.reason)
+            data = response.read()
+            conn.close()
+        except:
+            print("connection failed")
+        
+    def readTS(self):
+        data = requests.get(self.readAPI).json()
+        #feeds->last entry ->field1
+        result = data['feeds'][len(data['feeds'])-1]['field1']
+        return result
+    
+    def readCO2(self):
+        return random.random()*100
+    
+    def readMethane(self):
+        return random.random()*100
+    
+if __name__ == '__main__':
+    readAPI ='https://api.thingspeak.com/channels/1161317/fields/1.json?api_key=' + 'X3Q55V33X9YKOIYG' + '&results='
+    writeAPI = "L4GUMEDFAO5XU6OZ"
+    smokeDetectorPi = MQ2(readAPI,writeAPI)
+    smokeDetectorPi.establishConnection()
+    smokeDetectorPi.writeTS(smokeDetectorPi.readingString())
+    print(smokeDetectorPi.readTS())
+        
