279b399633a562eb78a009466644614a843c8d28
Andrew Cowan <andycowan@cmail.carleton.ca>
2020-12-07 14:20:11 -0500

    feat: updated code

--- Project/3010Project_SmokeDetectorPi.py
+++ Project/3010Project_SmokeDetectorPi.py
@@ -9,23 +9,27 @@ import serial
 
 class MQ2:
     def __init__(self,readAPI,writeAPI):
+        self.established = False
         self.methane = 0
         self.CO2 = 0
         self.readAPI = readAPI
         self.writeAPI = writeAPI
-        self.userID = '1'
-        self.sensorID = '2'
+        self.userID = '30'
+        self.sensorID = '3'
         self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         self.ser.flush()
     
     def establishConnection(self):
-        self.writeTS(self.userID+','+self.sensorID+','+'TRUE')
+        self.writeTS(self.userID+','+self.sensorID+','+'FALSE')
         time.sleep(5)
-        result = self.readTS()
-        if result == self.userID+','+self.sensorID+','+'TRUE':
-            print("Connection Established")
-        else:
-            print("Connection Failed")
+        while self.established == False:
+            result = self.readTS()
+            if result == self.userID+','+self.sensorID+','+'TRUE':
+                print("Connection Established")
+                self.established = True
+            else:
+                print("Connection Failed")
+            time.sleep(2)
     
     def writeTS(self,fieldString):
         params = urllib.urlencode({'field1': fieldString, 'key':self.writeAPI }) 
@@ -59,9 +63,9 @@ if __name__ == '__main__':
     readAPI ='https://api.thingspeak.com/channels/1161317/fields/1.json?api_key=' + 'X3Q55V33X9YKOIYG' + '&results='
     writeAPI = "L4GUMEDFAO5XU6OZ"
     smokeDetectorPi = MQ2(readAPI,writeAPI)
-    #smokeDetectorPi.establishConnection()
+    smokeDetectorPi.establishConnection()
     while True:
         smokeDetectorPi.writeTS(smokeDetectorPi.readSensor())
-        time.sleep(2)
+        time.sleep(5)
 
         
